93f11a3cc5e43e0cfaf49572fd9436b84887ba2c
EC2 Default User <specialized.19991129@gmail.com>
2023-04-16 18:57:30 +0900

    [Progate]cssを追加

---
+++ react-lesson/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Hello World</h1>
+
+        <p>一緒にReactを学びましょう！</p>
+      </div>
+    );
+  }
+}
+
+export default App;
